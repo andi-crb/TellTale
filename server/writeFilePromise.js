c2f8a93a6a638a5e38a6520b9dec19451e6f8e1c
Andi <andicbuchanan@gmail.com>
2016-02-25 11:13:07 +1300

    started adding ratings

---
+++ server/writeFilePromise.js
@@ -0,0 +1,15 @@
+
+var Promise = require('es6-promise').Promise
+var fs = require('fs')
+
+module.exports = function (filePath, file) {
+  return new Promise(function (resolve, reject) {
+    fs.writeFile(filePath, file, function (err, res) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(res)
+      }
+    })
+  })
+}
